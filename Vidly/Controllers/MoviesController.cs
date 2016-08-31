b1422081b5a539fc8529e25ca6e0e0c2741c971a
Andy Mulchek <a.mulchek@gmail.com>
2016-08-31 15:47:05 -0500

    initial commit

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+
+            return View(movie);
+            //return Content("Hello World!");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            //return RedirectToAction("Index", "Home", new { page= 1, sortBy = "name"});
+        }
+
+        //pass a Parameter to an Action
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        //Optional Parameters - need to make parameters nullable, add ? after int type, string by default is nullable
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "Name";
+            }
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+            // /movies for default values or overide with /movies?pageIndex=3,sortBy=ID
+        }
+
+        [Route("movies/released/{year}/{month:regex(\\d{4})}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+    }
+}
